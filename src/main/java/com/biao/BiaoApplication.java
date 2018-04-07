f18556e75e4a79bb6d8680b7bbdee8cd24dc6bca
vhuangyucheng <1063215202@qq.com>
2018-04-07 12:19:36 +0800

    Initial commit

---
+++ src/main/java/com/biao/BiaoApplication.java
@@ -0,0 +1,12 @@
+package com.biao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiaoApplication.class, args);
+	}
+}
